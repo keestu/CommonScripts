f4ae2e5ff3eb96df0cd8300165035c3b1b9e348a
keestu <keestu@gmail.com>
2025-10-18 17:25:05 +0800

    Refactor mouse location object and methods
    Simplify Mouse function to basics

--- mouseLoc.js
+++ mouseLoc.js
@@ -20,14 +20,8 @@ function defineMouseLocationObject() {
     var globMouseObj = new Object();
 
     /* Constructor for the global mouse location     */
-    globMouseObj.x     = 0;
-    globMouseObj.y     = 0;
-    globMouseObj.prevX = 0;
-    globMouseObj.prevY = 0;
-    globMouseObj.LR    = '';
-    globMouseObj.TB    = '';
-    globMouseObj.moveTime;
-    globMouseObj.responseRate = 100;
+    globMouseObj.x = 0;
+    globMouseObj.y = 0;
     document.onmousemove = function (e) {globMouseObj.mouseMove(e);};
 
     /* Getter for X and Y                            */
@@ -39,37 +33,17 @@ function defineMouseLocationObject() {
         return this.y;
         };
 
-    /* Getter for LRTB                               */
-    globMouseObj.getLR = function() {
-        return this.LR;
-        };
-
-    globMouseObj.getTB = function() {
-        return this.TB;
-        };
-
     /* Setter for X and Y                            */
     globMouseObj.setX = function(x) {
-        this.prevX = this.x;
-        this.x     = x;
+        this.x = x;
         };
 
     globMouseObj.setY = function(y) {
-        this.prevY = this.y
-        this.y     = y;
+        this.y = y;
         };
-
-    /* Setter for LRTB                               */
-    globMouseObj.setLR = function(LR) {
-        this.LR     = LR;
-        };
-
-    globMouseObj.setTB = function(TB) {
-        this.TB     = TB;
-        };
-
-    /* Set the mouse location                        */
-    globMouseObj.setLocation = function(evt) {
+        
+    globMouseObj.mouseMove = function(evt) {
+        evt = (evt) ? evt : (window.event) ? window.event : "";
         if (evt.pageX) {
             if (0 <= evt.pageX) {
                 this.setX(evt.pageX);
@@ -100,58 +74,12 @@ function defineMouseLocationObject() {
             }
         };
 
-    /* Set the LRTB                                  */
-    globMouseObj.setLRTB = function() {
-        if (this.x > this.prevX) {
-            globMouseObj.setLR("R");
-            }
-        else {
-            globMouseObj.setLR("L");
-            }
-
-        if (this.y > this.prevY) {
-            globMouseObj.setTB("B");
-            }
-        else {
-            globMouseObj.setTB("T");
-            }
-        };
-
-    /* No movement checker                           */
-    globMouseObj.checkMovement = function(evt){
-        globMouseObj.setLocation(evt);
-        if (this.x == this.prevX && this.y == this.prevY) {
-            globMouseObj.setLR("N");
-            globMouseObj.setTB("N");
-            }
-        };
-
-    globMouseObj.mouseMove = function(evt) {
-        evt = (evt) ? evt : (window.event) ? window.event : "";
-
-        globMouseObj.setLocation(evt);
-        globMouseObj.setLRTB();
-
-        clearTimeout(this.moveTime);
-        this.moveTime = setTimeout(function(){
-          globMouseObj.checkMovement(evt);
-          }, this.responseRate);
-        };
-
     /* Destructor                                    */
     globMouseObj.destroy = function() {
         printLog("    Destroy Mouse object");
         document.onmousemove = null;
-        clearInterval(this.moveTime);
-        delete this.x;
-        delete this.prevX
-        delete this.y;
-        delete this.prevY;
-        delete this.LR;
-        delete this.TB;
-        delete this.moveTime;
-        delete this.responseRate;
-        delete this;
+        this.x               = null;
+        this.y               = null;
         };
 
     return globMouseObj;
